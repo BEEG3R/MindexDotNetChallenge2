c170768601a2c8804e20da11fdae99c90010b7c7
AJ Frank <ybafrank@gmail.com>
2024-01-01 14:53:26 -0500

    Added Compensation service & repo

--- CodeChallenge/Models/Compensation.cs
+++ CodeChallenge/Models/Compensation.cs
@@ -5,9 +5,9 @@ namespace CodeChallenge.Models
     public class Compensation
     {
         public int Id { get; set; }
-        public int EmployeeId { get; set; }
+        public string EmployeeId { get; set; }
         public virtual Employee Employee { get; set; }
         public double Salary { get; set; }
-        public DateOnly EffectiveDate { get; set; }
+        public DateTime EffectiveDate { get; set; }
     }
 }
---
+++ CodeChallenge/Repositories/ICompensationRepository.cs
@@ -0,0 +1,13 @@
+﻿using CodeChallenge.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CodeChallenge.Repositories
+{
+    public interface ICompensationRepository
+    {
+        IEnumerable<Compensation> GetByEmployeeId(string id);
+        Compensation Add(Compensation compensation);
+        Task SaveAsync();
+    }
+}
