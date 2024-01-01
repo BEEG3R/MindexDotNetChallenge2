3cd3e2a6f428c0998e7a5cecaded518566ed81c4
AJ Frank <ybafrank@gmail.com>
2024-01-01 12:04:01 -0500

    Added new type and controller for new endpoint

---
+++ CodeChallenge/Models/ReportingStructure.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CodeChallenge.Models
+{
+    public class ReportingStructure
+    {
+        public ReportingStructure(Employee employee)
+        {
+            if (employee == null)
+            {
+                throw new ArgumentException("Employee cannot be null");
+            }
+            this.Employee = employee;
+        }
+
+        public Employee Employee { get; }
+        public int NumberOfReports
+        {
+            get
+            {
+                return GetAllReports(Employee);
+            }
+        }
+
+        private static int GetAllReports(Employee employee)
+        {
+            if (employee == null || employee.DirectReports == null)
+            {
+                return 0;
+            }
+            int sum = 0;
+            foreach (Employee e in employee.DirectReports)
+            {
+                sum++;
+                sum += GetAllReports(e);
+            }
+            return sum;
+        }
+    }
+}
